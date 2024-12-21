239c2da9bd9f8a4b68883aa7aab4e993cf35921b
fritolays505 <fritolays505@yahoo.com>
2024-12-20 19:03:55 -0500

    week 14

--- src/main/java/pet/store/PetStoreApplication.java
+++ src/main/java/pet/store/PetStoreApplication.java
@@ -1,11 +1,10 @@
-package com.promineotech.pet_store;
+package pet.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 
 @SpringBootApplication
-@EntityScan(basePackages = "pet.store.entity")
 public class PetStoreApplication {
 
 	public static void main(String[] args) {
